d5eef77af7add788ad28508eb5428c62107fca7b
chraebsli <nicholas.joel.krebs@gmail.com>
2023-06-04 15:33:31 +0200

    add dummy section at top

--- src/index.tsx
+++ src/index.tsx
@@ -72,6 +72,7 @@ function App() {
 			<Box sx={{height: "150px", marginTop: "3rem"}} />
 			<Container sx={{mt: "2rem", mb: "2rem"}}>
 				<main>
+					<section id="" style={{height: "1rem"}} />
 					{sections.map((section, index) => (
 						<React.Fragment key={index}>
 							{section}
