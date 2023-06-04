74c32319a738d06441fc556276a1de96a2d5b0e4
chraebsli <nicholas.joel.krebs@gmail.com>
2023-06-04 15:08:45 +0200

    auto refer header links on scroll

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,6 +5,7 @@ import { LanguageSwitcher } from "./LanguageSwitcher";
 import { useTranslation } from "react-i18next";
 import { Image } from "./Text";
 import { Sections } from "../../type/page";
+import React, { useEffect, useState } from "react";
 
 const sections = [
 	{key: "about", href: `#${Sections.About}`},
@@ -19,6 +20,61 @@ type Props = {
 }
 export default function Header({toggleTheme, checked}: Props) {
 	const {t} = useTranslation("components");
+
+	const initialSection = window?.location.href.split("#")[1];
+	const [activeSection, setActiveSection] = useState(initialSection || "#");
+
+	const handleLinkClick = (section: string) => {
+		setActiveSection(section);
+		scrollToSection(section);
+	};
+
+	const scrollToSection = (section: string) => {
+		const element = document.getElementById(section);
+		element?.scrollIntoView({behavior: "smooth"});
+	};
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollPosition = window.scrollY;
+			const sections = document.querySelectorAll("section");
+
+			sections.forEach((section) => {
+				const sectionTop = section.offsetTop;
+				const sectionHeight = section.offsetHeight;
+				const EM = 16;
+
+				if (
+					scrollPosition >= sectionTop - 6 * EM - 50 && // Adjust the offset as needed
+					scrollPosition < sectionTop + sectionHeight - 6 * EM - 50 // Adjust the offset as needed
+				) {
+					setActiveSection(section.id);
+				}
+			});
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	useEffect(() => {
+		const handleUrlAnchorChange = () => {
+			const sectionFromUrl = window.location.hash.substr(1);
+			setActiveSection(sectionFromUrl);
+		};
+
+		window.addEventListener("hashchange", handleUrlAnchorChange);
+		return () => {
+			window.removeEventListener("hashchange", handleUrlAnchorChange);
+		};
+	}, []);
+
+	useEffect(() => {
+		window.location.hash = activeSection;
+	}, [activeSection]);
+
 	return (
 		<AppBar position="fixed" enableColorOnDark>
 			<Container>
@@ -36,9 +92,9 @@ export default function Header({toggleTheme, checked}: Props) {
 						<Nav className="me-auto" />
 						<Nav>
 							{sections.map(page => (
-								<Nav.Link key={page.key} href={page.href}>
+								<Link key={page.key} href={page.href} active={activeSection === page.key} onClick={() => handleLinkClick(page.key)}>
 									{t(`header.links.${page.key}`)}
-								</Nav.Link>
+								</Link>
 							))}
 						</Nav>
 						<Stack direction="row" sx={{alignItems: "center", justifyContent: "start"}}>
@@ -49,5 +105,20 @@ export default function Header({toggleTheme, checked}: Props) {
 				</Navbar>
 			</Container>
 		</AppBar>
+	);
+}
+
+type LinkProps = {
+	children: React.ReactNode;
+	href: string;
+	onClick: () => void;
+	active: boolean;
+}
+
+function Link({children, href, onClick, active}: LinkProps) {
+	return (
+		<Nav.Link href={href} onClick={onClick} className={active ? "active" : ""}>
+			{children}
+		</Nav.Link>
 	);
 }
