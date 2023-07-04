ef2921630d39c606530e2c7373a8e34a261e728e
chraebsli <nicholas.joel.krebs@gmail.com>
2023-07-04 10:00:58 +0200

    refactor social.ts class

--- src/type/social.ts
+++ src/type/social.ts
@@ -1,17 +1,11 @@
 import React from "react";
 
 export class SocialItem {
-	href: string;
-	icon: React.ReactNode;
-	name: string;
-	user: string;
-	rel?: string;
-
-	constructor(href: string, icon: React.ReactNode, name: string, user: string, rel?: string) {
-		this.href = href;
-		this.icon = icon;
-		this.name = name;
-		this.user = user;
-		this.rel = rel;
-	}
+	constructor(
+		public readonly href: string,
+		public readonly icon: React.ReactNode,
+		public readonly name: string,
+		public readonly user: string,
+		public readonly rel?: string,
+	) {}
 }
