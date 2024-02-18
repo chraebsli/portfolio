3f5b1f14d6a448b20256a11fef676e8b45f74d3b
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-02-18 15:33:31 +0100

    fix: mobile timeline

--- src/components/experience/Timeline.tsx
+++ src/components/experience/Timeline.tsx
@@ -1,11 +1,19 @@
 import { default as MuiTimeline } from "@mui/lab/Timeline";
+import { useMediaQuery, useTheme } from "@mui/material";
+import { timelineOppositeContentClasses } from "@mui/lab";
 
 type Props = {
 	children: React.ReactNode;
 }
 export default function Timeline({children}: Props) {
+	const theme = useTheme();
+	const desktop = useMediaQuery(theme.breakpoints.up("md"));
+
 	return (
-		<MuiTimeline position="alternate">
+		<MuiTimeline
+			position={desktop ? "alternate" : "right"}
+			sx={desktop ? undefined : {[`& .${timelineOppositeContentClasses.root}`]: {flex: 0.2}}}
+		>
 			{children}
 		</MuiTimeline>
 	);
