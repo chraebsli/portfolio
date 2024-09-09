653b23c338406dfdf62999a92b33493350823546
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2024-09-09 13:22:50 +0200

    track socials

--- src/components/social/SocialItem.tsx
+++ src/components/social/SocialItem.tsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import { SocialItem as SMI } from "../../type/social";
+import umamiTrack from "../../tools/umamiTrack";
 
 type Props = {
 	item: SMI
 }
 export default function SocialItem({item}: Props) {
 	return (
-		<Box className="social-item">
+		<Box className="social-item" onClick={() => {umamiTrack("Social", {name: item.name});}}>
 			<a href={item.href} target="_blank" rel="noreferrer me">
 				<Box className="social-item-icon" sx={{width: "100%", color: "text.primary"}}>
 					{item.icon}
