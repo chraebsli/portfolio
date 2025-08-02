a720ffb5def8545a357e894aeff0a8e073ed0ea7
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2025-08-02 23:44:20 +0200

    update website content
    (cherry picked from commit 3127772c02d3ec0d0b8cae75f615740f169dbeb1)

--- src/locales/de.ts
+++ src/locales/de.ts
@@ -37,11 +37,10 @@ export default function de(): Translation {
 				title: "Über mich",
 				description: [
 					"Ich bin ein junger IT Enthusiast und Webentwickler aus der Schweiz, der sich stark für Webentwicklung und andere Themen im Bereich der IT interessiert. In meiner Freizeit programmiere ich gerne und stärke meine Fähigkeiten. Ausserdem bin ich in einer Brass Band, ein Pfadfinderleiter und im Winter ein Snowboarder.",
-					"Im August 2020 startete ich meine Informatikausbildung und lernte dort vieles in den Bereichen Netzwerk, IT-Sicherheit und Serveradministration. Es gab leider nur wenige Lerninhalte im Bereich der Programmierung, weshalb ich mich gelegentlich in meiner Freizeit damit auseinandergesetzt habe. Mit der Zeit interessierte ich mich immer mehr für die Programmierung und investierte anschliessend einen Grossteil meiner Freizeit, um mich privat weiterzubilden. Aktuell habe ich im letzten Semester meiner vierjährigen Ausbildung die Möglichkeit, selbstständig als Informatiker in meiner Firma zu arbeiten, als «Praktikum».",
-					"Mit dem Programmieren habe ich mit kleineren Python Programmen angefangen und anschliessend ein grösseres Spiel mit Python realisiert. Da ich danach etwas Neues ausprobieren wollte, kam ich zu PHP. In der Webentwicklung angekommen, fing ich an, mich immer stärker mit JavaScript auseinanderzusetzen. Seit Anfang 2023 beschäftige ich mich sehr stark mit TypeScript im Frontend sowie auch im Backend. Seit 2024 arbeite ich zudem erneut oft mit PHP für Kundenwebsites.",
-					"Bei der Fullstack Webentwicklung arbeite ich gerne mit React und Next.js im Frontend und mit Express.js und TypeORM im Backend. Für Kundenwebsites arbeite ich fast ausschliesslich mit dem Grav CMS, welches in PHP geschrieben wurde, weshalb ich mich immer mehr auch damit auseinandersetze.",
-					"Derzeit entwickle ich ein Grav Skeleton mit Themes und Plugins für zukünftige Kundenwebsites inklusive Dokumentation. Gleichzeitig pflege ich bestehende Websites und nehme auch gelegentlich Supportanfragen und diverse andere Aufträge an.",
-					"Welche Dienstleistungen ich anbiete, findest Du auf <a href='https://crabston.ch'>crabston.ch</a>.",
+					"Im August 2020 startete ich meine Informatikausbildung und lernte dort vieles in den Bereichen Netzwerk, IT-Sicherheit und Serveradministration. Es gab leider nur wenige Lerninhalte im Bereich der Programmierung, weshalb ich mich gelegentlich in meiner Freizeit damit auseinandergesetzt habe. Mit der Zeit interessierte ich mich immer mehr für die Programmierung und investierte anschliessend einen Grossteil meiner Freizeit, um mich privat weiterzubilden. Nun bin ich selbstständig und habe zusammen mit meinem Vater als Unterstützung meine eigene Firma gegründet, die Crabston GmbH. Auf der Website <a href='https://crabston.ch/?utm_source=nicholas-krebs.ch&utm_medium=portfolio'>crabston.ch</a> findet man die Dienstleistungen, die ich anbiete, und welche Projekte ich schon realisiert habe.",
+					"Mit dem Programmieren habe ich mit kleineren Python Programmen angefangen und anschliessend ein grösseres Spiel mit Python realisiert. Da ich danach etwas Neues ausprobieren wollte, kam ich zu PHP. In der Webentwicklung angekommen, fing ich an, mich immer stärker mit JavaScript auseinanderzusetzen. Seit Anfang 2023 beschäftige ich mich sehr stark mit TypeScript im Frontend sowie auch im Backend.",
+					"Bei der Fullstack Webentwicklung arbeite ich gerne mit React und Next.js im Frontend und mit Express.js und TypeORM im Backend. Für Websites arbeite ich ausschliesslich mit dem Grav CMS, einem PHP und Twig basierten flat-file CMS, weshalb ich mich immer mehr auch damit auseinandersetze.",
+					"Derzeit entwickle ich <a href='https://usehadron.dev/?utm_source=nicholas-krebs.ch&utm_medium=portfolio'>Hadron</a>, ein Grav Skeleton mit Theme, sowie Plugins für Kundenwebsites. Gleichzeitig pflege ich bestehende Websites und nehme auch gelegentlich Supportanfragen und diverse andere Aufträge entgegen.",
 				],
 			},
 			experience: {
@@ -51,7 +50,7 @@ export default function de(): Translation {
 					today: "heute",
 				},
 				subtitles: {
-					crabston: "Selbstständig als Informatiker (parallel zur Lehre)",
+					crabston: "Selbstständig als Web Entwickler",
 					tfbern: "Lehre als Informatiker EFZ",
 					gymnasium: "Gymnasium als 9. Klasse",
 					school: "7. & 8. Klasse Sekundarstufe",
--- src/locales/en.ts
+++ src/locales/en.ts
@@ -37,12 +37,10 @@ export default function en(): Translation {
 				title: "About me",
 				description: [
 					"I am a young IT enthusiast and web developer from Switzerland who is truly interested in web development and other topics in the field of IT. In my spare time, I enjoy programming and strengthening my skills. I'm also in a brass band, a scout leader and a snowboarder in the winter.",
-					"In August 2020, I started my IT apprenticeship and learned a lot about networks, IT security and server administration. Unfortunately, there was only a limited amount of learning content in the area of programming, which is why I occasionally dealt with it in my spare time. Over time, I became increasingly interested in programming and then invested a large part of my free time in continuing my personal education. Currently, in the last semester of my four-year apprenticeship, I have the opportunity to work independently as a computer scientist in my company as an “internship”.",
-					"I started programming with smaller Python programs and then created a larger game with Python. Since I wanted to try something new, I came to PHP. Once I got into web development, I started to get more and more involved with JavaScript. Since the beginning of 2023 I have been working very frequently with TypeScript in the frontend and backend. Since 2024, I have also regularly been working with PHP for customer websites.",
-					"For full stack web development, I like to work with React and Next.js on the frontend and with Express.js and TypeORM on the backend. For customer websites, I work almost exclusively with the Grav CMS, which was written in PHP, which is why I'm getting more and more involved with it.",
-					"I am currently developing a Grav skeleton with themes and plugins for future customer websites, including documentation. At the same time, I maintain existing websites and occasionally accept support requests and various other assignments.",
-					"You can find out which services I offer at <a href='https://crabston.ch'>crabston.ch</a>.",
-
+					"In August 2020, I started my IT apprenticeship and learned a lot about networks, IT security and server administration. Unfortunately, there was only a limited amount of learning content in the area of programming, which is why I occasionally dealt with it in my spare time. Over time, I became increasingly interested in programming and then invested a large part of my free time in continuing my personal education. Now I am self-employed and, with the support of my father, I founded my own company, Crabston GmbH. On the website <a href='https://crabston.ch/?utm_source=nicholas-krebs.ch&utm_medium=portfolio'>crabston.ch</a>, you can find the services I offer and the projects I've already completed.",
+					"I started programming with smaller Python programs and then created a larger game with Python. Since I wanted to try something new, I came to PHP. Once I got into web development, I started to get more and more involved with JavaScript. Since the beginning of 2023 I have been working very frequently with TypeScript in the frontend and backend.",
+					"For full stack web development, I like to work with React and Next.js on the frontend and with Express.js and TypeORM on the backend. For websites, I work exclusively with the Grav, a flat-file CMS written in PHP using Twig as a templating engine, which is why I'm getting more and more involved with it.",
+					"I am currently developing <a href='https://usehadron.dev/?utm_source=nicholas-krebs.ch&utm_medium=portfolio'>Hadron</a>, a Grav skeleton with theme, and plugins for customer websites. At the same time, I maintain existing websites and occasionally accept support requests and various other jobs.",
 				],
 			},
 			experience: {
@@ -52,7 +50,7 @@ export default function en(): Translation {
 					today: "today",
 				},
 				subtitles: {
-					crabston: "Self-employed as a computer scientist (parallel to apprentice)",
+					crabston: "Self-employed as a web developer",
 					tfbern: "Apprenticeship as a computer specialist EFZ",
 					gymnasium: "Gymnasium as 9th grade",
 					school: "Secondary school 7th & 8th grade",
